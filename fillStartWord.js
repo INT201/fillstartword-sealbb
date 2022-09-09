ec239ae5095a34ba9c8e9263000396ccef0cdd1d
Chonticha <Chonticha.it@mail.kmutt.ac.th>
2022-09-10 00:49:27 +0700

    finish

--- fillStartWord.js
+++ fillStartWord.js
@@ -2,5 +2,12 @@ const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
   //code here
+  if(word===null || word===undefined){
+    return undefined
+  } if(word.includes(startWord)){
+    return word
+  }else{
+    return startWord+word
+  }
 }
 module.exports = fillStartWord
